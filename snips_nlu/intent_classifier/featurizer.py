6fc6cb3e2ea26f5bb17feb29cdbf153ac1e49de1
LukeS26 <LukeS062022@gmail.com>
2023-11-09 21:54:27 -0500

    test

--- snips_nlu/intent_classifier/featurizer.py
+++ snips_nlu/intent_classifier/featurizer.py
@@ -334,6 +334,7 @@ class TfidfVectorizer(ProcessingUnit):
         utterances = [self._enrich_utterance(*data)
                       for data in zip(*self._preprocess(x))]
         utterances = self._tfidf_vectorizer.fit(utterances)
+        print(utterances.shape())
         return self._tfidf_vectorizer.transform(utterances)
 
     def _preprocess(self, utterances):
